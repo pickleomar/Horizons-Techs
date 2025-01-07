1a9e0c21af32b0ee1673257c83af19419274123c
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-07 23:52:06 +0100

    chore: remove duplicate fields registration_date(), fields already exist created_at.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,8 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
-            $table->enum('type',['Guest','Subscriber','Manager','Editor']);
-            $table->dateTime('registration_date');
+            $table->enum('type', ['Guest', 'Subscriber', 'Manager', 'Editor']);
             $table->rememberToken();
             $table->timestamps();
         });
