0a42cdb918f257f62d4e82f06c5a22c0d8a533ba
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-07 23:30:00 +0100

    chore: add relationships the the Issue Model.

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,14 @@ class Rating extends Model
 {
 
     protected $fillable = ['user_id', 'article_id', 'rating'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function  article()
+    {
+        return $this->belongsTo(Article::class);
+    }
 }
