cbf57c0d091d0ab89f81b9405aeef39d971292fb
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-10 01:28:11 +0100

    chore: Create Factory for the History Model.

---
+++ database/factories/HistoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\History>
+ */
+class HistoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "user_id" => User::factory(),
+            "article_id" => Article::factory(),
+            "consultation_date" => fake()->optional()->dateTime,
+        ];
+    }
+}
