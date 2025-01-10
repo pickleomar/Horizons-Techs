26673c2a1fb745b5b6933d4c027652652d482c9b
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-10 01:56:14 +0100

    feat: create seeding function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Article;
+use App\Models\Chat;
+use App\Models\History;
+use App\Models\Issue;
+use App\Models\Rating;
+use App\Models\Subscription;
 use App\Models\Theme;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,8 +22,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory(10)->create();
-        Theme::factory(5)->create();
-        Article::factory(20)->create();
+        User::factory(20)->create();
+        Theme::factory(10)->create();
+        Article::factory(30)->create();
+        Rating::factory(20)->create();
+        Chat::factory(10)->create();
+        Subscription::factory(10)->create();
+        History::factory(10)->create();
     }
 }
