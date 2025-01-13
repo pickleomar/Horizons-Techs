d814985812e7f15f012f8ab21eea6a4bcdcc7720
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-13 02:31:12 +0100

    feat: add all the themes routes.

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,24 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-
+// Articles Routes
 Route::get('/articles', [ArticleController::class, "index"])->middleware(["auth", "verified"])->name('articles.index');
 Route::get('/articles/create', [ArticleController::class, "create"])->middleware(["auth", "verified"])->name('article.create');
 Route::post('/articles/create', [ArticleController::class, "store"])->middleware(["auth", "verified"]);
-
-
-
-
 Route::get('/articles/{article}', [ArticleController::class, "show"]);
+
+// Themes Routes
+Route::get("themes", [ThemeController::class, "index"])->name("themes.index");
+Route::get("themes/create", [ThemeController::class, "create"])->name("themes.create");
+Route::post("themes/create", [ThemeController::class, "store"]);
+Route::get("themes/{theme}", [ThemeController::class, "show"])->name("themes.show");
 
 
 Route::get('/dashboard', function () {
