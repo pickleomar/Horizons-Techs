fe61200677864f5a6a0a7fa1806a5bfa813694e6
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-16 19:43:54 +0100

    feature: theme creation working just fine.

--- resources/views/themes/create.blade.php
+++ resources/views/themes/create.blade.php
@@ -1,13 +1,25 @@
 <x-app-layout>
-    <form method="POST" action="{{ route('themes.create') }}">
+    <form method="POST" action="{{ route('themes.create') }}" enctype="multipart/form-data">
         @csrf
 
 
         <label for="">Name</label><br>
-        <input type="text" placeholder="Article title ..." name="name"><br>
+        <input type="text" placeholder="Theme name ..." name="name"><br>
+        <div style="color: #F14336">
+            {{ implode('', $errors->get('name')) }}
+        </div>
 
+        <label for="">Image</label><br>
+        <input type="file" accept="image/png, image/jpeg" name="image" id="image"><br>
+
+        <div style="color: #F14336">
+            {{ implode('', $errors->get('image')) }}
+        </div>
         <label for="">Description</label><br>
-        <textarea name="description" placeholder="Content title ..."></textarea><br>
+        <textarea name="description" placeholder="description  ..."></textarea><br>
+        <div style="color: #F14336">
+            {{ implode('', $errors->get('description')) }}
+        </div>
 
         <button type="submit">
             Submit
--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -2,9 +2,9 @@
     <div class="themes-container">
         @foreach ($themes as $theme)
             <div class="theme-card">
-                {{-- <img src="{{ $theme->image }}" alt="{{ $theme->name }}"> --}}
-                <img src="https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="{{ $theme->name }}">
+                <img src="{{ asset($theme->image) }}" alt="{{ $theme->name }}">
+                {{-- <img src="https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="{{ $theme->name }}"> --}}
                 <h2>{{ $theme->name }}</h2>
                 <p>{{ $theme->description }}</p>
                 <x-button class="btn-primary"
