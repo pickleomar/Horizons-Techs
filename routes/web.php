8693432c608e887a110033a49de7193911a4616d
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-18 18:12:13 +0100

    feature: add new route delete theme.

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,12 @@ Route::get('/articles/{article}', [ArticleController::class, "show"]);
 Route::get("/themes", [ThemeController::class, "index"])->name("themes.index");
 Route::get("/themes/create", [ThemeController::class, "create"])->name("themes.create");
 Route::post("/themes/create", [ThemeController::class, "store"]);
-Route::get("/themes/{theme}", [ThemeController::class, "show"])->name("themes.show");
 
+// View Theme
+Route::get("/themes/{theme}", [ThemeController::class, "show"])->name("themes.show");
+// Delete Themes
+Route::delete("/themes/{theme}", [ThemeController::class, "destroy"])->name("themes.destroy");
+// View Theme Articles
 Route::get('themes/{theme}/articles', [ArticleController::class, "index"])->middleware(["auth", "verified"])->name('articles.index');
 Route::get('themes/{theme}/articles/create', [ArticleController::class, "create"])->middleware(["auth", "verified"])->name('article.create');
 Route::post('themes/{theme}/articles/create', [ArticleController::class, "store"])->middleware(["auth", "verified"]);
