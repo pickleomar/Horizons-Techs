bd7cc50c9bc2647669d88ae18256f714f91578c3
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-19 22:43:58 +0100

    feat: bind interfaces and implementation.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\ArticleRepository;
+use App\Repositories\ArticleRepositoryInterface;
 use App\Repositories\ThemeRepository;
 use App\Repositories\ThemeRepositoryInterface;
+use App\Services\ArticleService;
+use App\Services\ArticleServiceInterface;
 use App\Services\ThemeService;
 use App\Services\ThemeServiceInterface;
 use Illuminate\Support\ServiceProvider;
@@ -15,8 +19,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+
+
+        // Themes Binding
         $this->app->bind(ThemeRepositoryInterface::class, ThemeRepository::class);
         $this->app->bind(ThemeServiceInterface::class, ThemeService::class);
+        // Articles Binding
+        $this->app->bind(ArticleRepositoryInterface::class, ArticleRepository::class);
+        $this->app->bind(ArticleServiceInterface::class, ArticleService::class);
     }
 
     /**
