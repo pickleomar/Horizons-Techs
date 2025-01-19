72d3e3b3496afa76ebe228f338fdedfed5fb0fb4
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-19 02:07:05 +0100

    feat: add Binding for the ThemeService.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\ThemeRepository;
 use App\Repositories\ThemeRepositoryInterface;
+use App\Services\ThemeService;
+use App\Services\ThemeServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ThemeRepositoryInterface::class, ThemeRepository::class);
+        $this->app->bind(ThemeServiceInterface::class, ThemeService::class);
     }
 
     /**
