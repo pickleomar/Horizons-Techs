03991df6e01103aa08531533a8b155c5c51a719e
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-22 14:38:43 +0100

    chore: remove bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,22 +2,6 @@
 
 namespace App\Providers;
 
-use App\Repositories\ArticleRepository;
-use App\Repositories\ArticleRepositoryInterface;
-use App\Repositories\HistoryRepository;
-use App\Repositories\HistoryRepositoryInterface;
-use App\Repositories\SubscriptionRepository;
-use App\Repositories\SubscriptionRepositoryInterface;
-use App\Repositories\ThemeRepository;
-use App\Repositories\ThemeRepositoryInterface;
-use App\Services\ArticleService;
-use App\Services\ArticleServiceInterface;
-use App\Services\HistoryService;
-use App\Services\HistoryServiceInterface;
-use App\Services\SubscriptionService;
-use App\Services\SubscriptionServiceInterface;
-use App\Services\ThemeService;
-use App\Services\ThemeServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,12 +9,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        // Articles Binding
-        $this->app->bind(ArticleRepositoryInterface::class, ArticleRepository::class);
-        $this->app->bind(ArticleServiceInterface::class, ArticleService::class);
-    }
+    public function register(): void {}
 
 
 
