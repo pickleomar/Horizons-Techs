b92253577d827c9f2fe3758b8caa8b401014f580
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-24 23:58:42 +0100

    feature: enhance themes page general look.

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -13,17 +13,46 @@
 
 
     <div class="themes-container">
-        @foreach ($themes as $theme)
-            <div class="theme-card">
-                <img src="{{ str_starts_with($theme->image, 'http') ? $theme->image : asset($theme->image) }}"
-                    alt="{{ $theme->name }}">
-                {{-- <img src="https://plus.unsplash.com/premium_photo-1682145730713-34bba6d3d14a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="{{ $theme->name }}"> --}}
-                <h2>{{ $theme->name }}</h2>
-                <p>{{ $theme->description }}</p>
-                <x-button class="btn-primary"
-                    href="{{ route('themes.show', ['theme' => $theme->id]) }}">Discover</x-button>
+        <header class="header">
+            <div class="header-content">
+                <h1 class="page-title">Discover themes</h1>
+                <x-button href="{{ route('themes.create') }}" class="new-theme-btn">+ New theme</x-button>
             </div>
-        @endforeach
+        </header>
+
+        <div class="filters">
+            <button class="filter-btn active">All</button>
+            <button class="filter-btn">Popular</button>
+            <button class="filter-btn">Recent</button>
+            <button class="filter-btn">Featured</button>
+        </div>
+
+        <main class="themes-grid">
+
+
+            @foreach ($themes as $theme)
+                <article class="theme-card">
+                    <img src="{{ str_starts_with($theme->image, 'http') ? $theme->image : asset($theme->image) }}"
+                        alt="{{ $theme->name }}">
+                    <div class="theme-content">
+                        <a style="all: unset;cursor: pointer;"
+                            href="{{ route('themes.show', ['theme' => $theme->id]) }}">
+
+
+                            <h2 class="theme-title"> {{ $theme->name }}</h2>
+                            <p class="theme-description">
+                                {{ $theme->description }}
+                            </p>
+                            <div class="theme-metadata">
+                                <span>{{ $theme->manager->name }}</span>
+                                <span>{{ $theme->created_at }}</span>
+                            </div>
+                    </div>
+                    </a>
+                </article>
+            @endforeach
+
+
+        </main>
     </div>
 </x-app-layout>
