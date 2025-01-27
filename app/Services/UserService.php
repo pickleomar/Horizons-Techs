50edfd9fa50964f29ec1e27857ee5cf5907c00bf
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-27 21:42:46 +0100

    feature: add repository and service for the user.

---
+++ app/Services/UserService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+
+class UserService
+{
+    protected $userRepository;
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAllUsers()
+    {
+        return $this->userRepository->all();
+    }
+    public function getUserById($id)
+    {
+        return $this->userRepository->find($id);
+    }
+    public function createUser(array $data)
+    {
+        return $this->userRepository->create($data);
+    }
+    public function updateUser($id, array $data)
+    {
+        return $this->userRepository->update($id, $data);
+    }
+    public function deleteUser($id)
+    {
+        return $this->userRepository->delete($id);
+    }
+}
