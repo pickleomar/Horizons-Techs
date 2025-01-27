1a8eaa626699f83295517af438ee64a580cea9ba
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-27 21:42:06 +0100

    feature: add bio to the user model and configure controller for profile.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,59 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileUpdateRequest;
-use Illuminate\Http\RedirectResponse;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
-    public function edit(Request $request): View
+
+    protected $userService;
+
+
+    public function __construct(UserService $userService)
     {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
+        $this->userService = $userService;
     }
 
-    /**
-     * Update the user's profile information.
-     */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function index(Request $request)
     {
-        $request->user()->fill($request->validated());
+        return view("dashboard.profile");
+    }
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+
+    public function update(Request $request)
+    {
+
+        $request->validate([
+            "name" => "string|max:255",
+            "avatar" => "image"
+        ]);
+
+        $data = [];
+        $avatarName = "";
+
+        if ($request->avatar) {
+            $avatarName = time() . '.' . $request->avatar->extension();
+            $request->avatar->move(public_path('avatars'), $avatarName);
+
+            $data = [
+                "name" => $request->name,
+                "bio" => $request->bio,
+                "avatar" => "avatars/$avatarName"
+            ];
+        } else {
+
+            $data = [
+                "name" => $request->name,
+                "bio" => $request->bio,
+            ];
         }
 
-        $request->user()->save();
+        $this->userService->updateUser(Auth::user()->id, $data);
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }
 
-    /**
-     * Delete the user's account.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
-
-        $user = $request->user();
-
-        Auth::logout();
-
-        $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return Redirect::to('/');
+        return redirect()->route("dashboard.profile");
     }
 }
