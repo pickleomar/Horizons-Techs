296473da4e9a1badc74627b9f4ae616b68b7ef55
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-01-28 03:07:11 +0100

    migrations: recreate migration for the history table.

--- database/migrations/2025_01_28_020535_recreate_histoty_table.php
+++ database/migrations/2025_01_28_020535_recreate_histoty_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('history');
         Schema::create('history', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete("cascade");
