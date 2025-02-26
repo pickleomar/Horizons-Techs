d593e43d684c0d25c2cffd20bc4435da1ce36c0f
Omar Azzaoui <56740409+pickleomar@users.noreply.github.com>
2025-02-26 02:54:56 +0000

    Theme creation authorization fixed and favicon linking

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <title>Horizons Techs</title>
 
 
     <!-- Scripts -->
